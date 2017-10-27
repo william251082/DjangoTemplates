af2e1a5b6507df0fe4718c8762a2d68f19199152
williamdelrosario <williamdelrosario@yahoo.com>
2017-10-27 13:10:15 +0200

    Template Filters

---
+++ basic_app/templatetags/my_extras.py
@@ -0,0 +1,11 @@
+my django import template
+
+register = template.Library()
+
+def cut(value,arg):
+    """
+    This cuts out all values of "arg" from the string!
+    """
+    return value.replace(arg,'')
+
+register.filter('cut',cut)
